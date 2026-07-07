555e7fa78121c7f590c491463a0a7782a9e69e4c
pavankalyan6659 <vemulapavankalyan887@gmail.com>
2026-07-07 14:53:25 +0530

    Updated ImageBanner button design and styling

--- src/components/layout/ImageBanner.jsx
+++ src/components/layout/ImageBanner.jsx
@@ -6,7 +6,12 @@ export default function ImageBanner({ onNavigate }) {
       <div className="image-banner-container">
         <img src="/images/brand/og-banner.png" alt="OG Street Wear Banner" />
         <div className="image-banner-overlay">
-          <button className="btn primary" onClick={() => onNavigate('collections')}>Explore Collections</button>
+         <button
+  className="explore-collection-btn"
+  onClick={() => onNavigate("collections")}
+>
+  Explore Collections
+</button>
         </div>
       </div>
     </section>
