376fdfa0828cadc9dc0dc03bfa67938ea43b165e
pavankalyan6659 <vemulapavankalyan887@gmail.com>
2026-07-28 10:33:04 +0530

    Migrate theme and auth state to Redux Toolkit

---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "../features/theme/themeSlice";
+import authReducer from "../features/auth/authSlice";
+
+
+export const store = configureStore({
+  reducer: {
+    theme: themeReducer,
+    auth: authReducer,
+  },
+});
